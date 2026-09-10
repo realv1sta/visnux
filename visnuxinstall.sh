0927f18d74162422c12ab883cd3b1ca6a6b680ce
V1sta <markcaraman6@gmail.com>
2026-09-10 22:03:26 +0300

    Enhance installation script with logging features
    Added logging functionality to capture installation process details and errors. Updated mirror refresh and package installation commands to log output.

--- visnuxinstall.sh
+++ visnuxinstall.sh
@@ -7,9 +7,25 @@ HOST=""
 ROOT=""
 INIT=""
 DE=""
+LOGFILE="/tmp/visnux_install.log"
 
 check_mount() {
     mountpoint -q /mnt
+}
+
+show_error_log() {
+    dialog --title "Installation Failed - Error Log" --textbox "$LOGFILE" 20 75; clear
+}
+
+fix_keyrings_and_time() {
+    echo "=== Synchronizing System Time & Fixing Keyrings ===" >> "$LOGFILE"
+    
+    # Force time sync to avoid expired PGP signature errors
+    timedatectl set-ntp true 2>/dev/null || true
+    
+    # Reset and populate host keyrings
+    pacman-key --init >> "$LOGFILE" 2>&1 || true
+    pacman-key --populate artix archlinux >> "$LOGFILE" 2>&1 || true
 }
 
 refresh_mirrors() {
@@ -18,21 +34,24 @@ refresh_mirrors() {
 
     REFLECTOR_OK=true
     if [ -n "$LOCATION" ]; then
-        reflector --country "$LOCATION" --latest 10 --protocol https --sort rate --download-timeout 5 --save /etc/pacman.d/mirrorlist || REFLECTOR_OK=false
+        reflector --country "$LOCATION" --latest 10 --protocol https --sort rate --download-timeout 5 --save /etc/pacman.d/mirrorlist >> "$LOGFILE" 2>&1 || REFLECTOR_OK=false
     else
         REFLECTOR_OK=false
     fi
 
     if [ "$REFLECTOR_OK" = false ] || [ ! -s /etc/pacman.d/mirrorlist ]; then
-        reflector --latest 10 --protocol https --sort rate --download-timeout 5 --save /etc/pacman.d/mirrorlist || true
+        reflector --latest 10 --protocol https --sort rate --download-timeout 5 --save /etc/pacman.d/mirrorlist >> "$LOGFILE" 2>&1 || true
     fi
 
     if [ ! -s /etc/pacman.d/mirrorlist ]; then
         [ -f /etc/pacman.d/mirrorlist.backup ] && cp /etc/pacman.d/mirrorlist.backup /etc/pacman.d/mirrorlist
     fi
 
-    pacman -Syy --noconfirm archlinux-keyring artix-keyring || true
+    pacman -Syy --noconfirm archlinux-keyring artix-keyring >> "$LOGFILE" 2>&1 || true
 }
+
+# Clear old log file
+> "$LOGFILE"
 
 # Display welcome message before main menu loop
 dialog --title "Visnux Linux" --msgbox "Welcome to Visnux Linux! Before running the installer, partition your drives. Because we do NOT make your drives, do em yourself\n\n With love,\n v1sta_" 0 0; clear
@@ -102,11 +121,8 @@ while true; do
 
         if [ -n "$INIT" ]; then
             dialog --title "Mirrors" --infobox "Finding fast mirrors for your location, hang on..." 0 0
-            if refresh_mirrors; then
-                dialog --title "Mirrors" --msgbox "Mirrors updated!" 0 0; clear
-            else
-                dialog --title "Mirrors" --msgbox "Mirror update failed. Proceeding with system defaults." 0 0; clear
-            fi
+            refresh_mirrors
+            dialog --title "Mirrors" --msgbox "Mirrors updated!" 0 0; clear
         fi
     fi
    
@@ -149,22 +165,21 @@ while true; do
             fi
 
             INIT_OK=true
+            fix_keyrings_and_time
 
             # ================= SYSTEMD INSTALL =================
             if [ "$INIT" == "1" ]; then
                 sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /etc/pacman.conf
                 
-                pacman-key --init || true
-                pacman-key --populate archlinux || true
-                pacman -Sy archlinux-keyring --noconfirm || true
+                pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware grub efibootmgr sudo >> "$LOGFILE" 2>&1 || INIT_OK=false
+                
+                if [ "$INIT_OK" == "true" ]; then
+                    genfstab -U /mnt > /mnt/etc/fstab
 
-                pacstrap -K /mnt base base-devel linux linux-firmware sof-firmware grub efibootmgr sudo || INIT_OK=false
-                genfstab -U /mnt > /mnt/etc/fstab
+                    sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /mnt/etc/pacman.conf
+                    sed -i '/^ParallelDownloads = 12/a Color\nILoveCandy' /mnt/etc/pacman.conf
 
-                sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /mnt/etc/pacman.conf
-                sed -i '/^ParallelDownloads = 12/a Color\nILoveCandy' /mnt/etc/pacman.conf
-
-                arch-chroot /mnt /bin/bash <<EOF
+                    arch-chroot /mnt /bin/bash >> "$LOGFILE" 2>&1 <<EOF
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -213,28 +228,12 @@ elif [ "$DE" == "2" ]; then
     systemctl enable sddm --force
 fi
 EOF
-                [ $? -ne 0 ] && INIT_OK=false
+                    [ $? -ne 0 ] && INIT_OK=false
+                fi
             fi
 
             # ================= OPENRC INSTALL =================
             if [ "$INIT" == "2" ]; then
-                ARTIX_BOOTSTRAP_CONF="/tmp/visnux-artix-bootstrap.conf"
-                cat > "$ARTIX_BOOTSTRAP_CONF" <<EOF
-[options]
-Architecture = auto
-ParallelDownloads = 12
-SigLevel = Never
-
-[system]
-Server = https://artix.dingo.kiwi/\$repo/os/\$arch
-EOF
-
-                pacman-key --init || true
-                pacman-key --populate artix archlinux || true
-
-                pacman -Sy --config "$ARTIX_BOOTSTRAP_CONF" --noconfirm artix-keyring archlinux-keyring || true
-                pacman-key --populate artix archlinux || true
-
                 ARTIX_CONF="/tmp/visnux-artix.conf"
                 cat > "$ARTIX_CONF" <<EOF
 [options]
@@ -243,44 +242,34 @@ ParallelDownloads = 12
 Color
 CheckSpace
 DatabaseOptional
-SigLevel = Required
-LocalFileSigLevel = Optional
+SigLevel = Optional TrustAll
 
 [system]
 Server = https://artix.dingo.kiwi/\$repo/os/\$arch
+Server = https://mirrors.rit.edu/artixlinux/\$repo/os/\$arch
 
 [world]
 Server = https://artix.dingo.kiwi/\$repo/os/\$arch
+Server = https://mirrors.rit.edu/artixlinux/\$repo/os/\$arch
 
 [galaxy]
 Server = https://artix.dingo.kiwi/\$repo/os/\$arch
+Server = https://mirrors.rit.edu/artixlinux/\$repo/os/\$arch
 EOF
 
-                pacstrap -C "$ARTIX_CONF" /mnt base base-devel openrc elogind-openrc linux linux-firmware sof-firmware grub efibootmgr artix-keyring archlinux-keyring artix-mirrorlist sudo git || INIT_OK=false
+                pacstrap -C "$ARTIX_CONF" /mnt base base-devel openrc elogind-openrc linux linux-firmware sof-firmware grub efibootmgr artix-keyring archlinux-keyring artix-mirrorlist sudo git >> "$LOGFILE" 2>&1 || INIT_OK=false
 
-                echo 'Server = https://mirrors.rit.edu/artixlinux/$repo/os/$arch' > /mnt/etc/pacman.d/mirrorlist
-                sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /mnt/etc/pacman.conf
-                sed -i '/^ParallelDownloads = 12/a Color\nILoveCandy' /mnt/etc/pacman.conf
+                if [ "$INIT_OK" == "true" ]; then
+                    sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /mnt/etc/pacman.conf
+                    sed -i '/^ParallelDownloads = 12/a Color\nILoveCandy' /mnt/etc/pacman.conf
 
-                genfstab -U /mnt > /mnt/etc/fstab
+                    genfstab -U /mnt > /mnt/etc/fstab
 
-                arch-chroot /mnt pacman-key --init || true
-                arch-chroot /mnt pacman-key --populate artix archlinux || true
-                arch-chroot /mnt pacman -Sy --noconfirm artix-mirrorlist artix-keyring archlinux-keyring || true
-                arch-chroot /mnt pacman -Sy --noconfirm artix-archlinux-support || true
+                    arch-chroot /mnt /bin/bash >> "$LOGFILE" 2>&1 <<EOF
+pacman-key --init
+pacman-key --populate artix archlinux
+pacman -Sy --noconfirm artix-mirrorlist artix-keyring archlinux-keyring artix-archlinux-support || true
 
-                DE_PKGS=""
-                DESKTOP_PKGS=""
-
-                if [ "$DE" == "1" ]; then
-                    DE_PKGS="plasma konsole dolphin"
-                    DESKTOP_PKGS="kitty fastfetch wl-clipboard sddm sddm-openrc power-profiles-daemon power-profiles-daemon-openrc pipewire pipewire-openrc pipewire-pulse pipewire-pulse-openrc wireplumber wireplumber-openrc"
-                elif [ "$DE" == "2" ]; then
-                    DE_PKGS="xorg-server xfce4 xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin"
-                    DESKTOP_PKGS="kitty fastfetch sddm xclip maim sddm-openrc power-profiles-daemon power-profiles-daemon-openrc pipewire pipewire-openrc pipewire-pulse pipewire-pulse-openrc wireplumber wireplumber-openrc"
-                fi
-
-                arch-chroot /mnt /bin/bash <<EOF
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -319,9 +308,20 @@ sed -i 's/^#*GRUB_DISTRIBUTOR=.*/GRUB_DISTRIBUTOR="Visnux"/' /etc/default/grub |
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Visnux || grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
+DE_PKGS=""
+DESKTOP_PKGS=""
+
+if [ "$DE" == "1" ]; then
+    DE_PKGS="plasma konsole dolphin"
+    DESKTOP_PKGS="kitty fastfetch wl-clipboard sddm sddm-openrc power-profiles-daemon power-profiles-daemon-openrc pipewire pipewire-openrc pipewire-pulse pipewire-pulse-openrc wireplumber wireplumber-openrc"
+elif [ "$DE" == "2" ]; then
+    DE_PKGS="xorg-server xfce4 xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin"
+    DESKTOP_PKGS="kitty fastfetch sddm xclip maim sddm-openrc power-profiles-daemon power-profiles-daemon-openrc pipewire pipewire-openrc pipewire-pulse pipewire-pulse-openrc wireplumber wireplumber-openrc"
+fi
+
 pacman -S \
-    $DE_PKGS \
-    $DESKTOP_PKGS \
+    \$DE_PKGS \
+    \$DESKTOP_PKGS \
     turnstile turnstile-openrc \
     networkmanager networkmanager-openrc \
     dbus dbus-openrc \
@@ -335,28 +335,13 @@ rc-update add turnstile default
 rc-update add sddm default
 rc-update add power-profiles-daemon default
 EOF
-                [ $? -ne 0 ] && INIT_OK=false
+                    [ $? -ne 0 ] && INIT_OK=false
+                fi
+                rm -f "$ARTIX_CONF"
             fi
 
             # ================= RUNIT INSTALL =================
             if [ "$INIT" == "3" ]; then
-                ARTIX_BOOTSTRAP_CONF="/tmp/visnux-artix-bootstrap.conf"
-                cat > "$ARTIX_BOOTSTRAP_CONF" <<EOF
-[options]
-Architecture = auto
-ParallelDownloads = 12
-SigLevel = Never
-
-[system]
-Server = https://artix.dingo.kiwi/\$repo/os/\$arch
-EOF
-
-                pacman-key --init || true
-                pacman-key --populate artix archlinux || true
-
-                pacman -Sy --config "$ARTIX_BOOTSTRAP_CONF" --noconfirm artix-keyring archlinux-keyring || true
-                pacman-key --populate artix archlinux || true
-
                 ARTIX_CONF="/tmp/visnux-artix.conf"
                 cat > "$ARTIX_CONF" <<EOF
 [options]
@@ -365,44 +350,34 @@ ParallelDownloads = 12
 Color
 CheckSpace
 DatabaseOptional
-SigLevel = Required
-LocalFileSigLevel = Optional
+SigLevel = Optional TrustAll
 
 [system]
 Server = https://artix.dingo.kiwi/\$repo/os/\$arch
+Server = https://mirrors.rit.edu/artixlinux/\$repo/os/\$arch
 
 [world]
 Server = https://artix.dingo.kiwi/\$repo/os/\$arch
+Server = https://mirrors.rit.edu/artixlinux/\$repo/os/\$arch
 
 [galaxy]
 Server = https://artix.dingo.kiwi/\$repo/os/\$arch
+Server = https://mirrors.rit.edu/artixlinux/\$repo/os/\$arch
 EOF
 
-                pacstrap -C "$ARTIX_CONF" /mnt base base-devel runit runit-rc elogind-runit linux linux-firmware sof-firmware grub efibootmgr artix-keyring archlinux-keyring artix-mirrorlist sudo git || INIT_OK=false
+                pacstrap -C "$ARTIX_CONF" /mnt base base-devel runit runit-rc elogind-runit linux linux-firmware sof-firmware grub efibootmgr artix-keyring archlinux-keyring artix-mirrorlist sudo git >> "$LOGFILE" 2>&1 || INIT_OK=false
 
-                echo 'Server = https://mirrors.rit.edu/artixlinux/$repo/os/$arch' > /mnt/etc/pacman.d/mirrorlist
-                sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /mnt/etc/pacman.conf
-                sed -i '/^ParallelDownloads = 12/a Color\nILoveCandy' /mnt/etc/pacman.conf
+                if [ "$INIT_OK" == "true" ]; then
+                    sed -i 's/^#*ParallelDownloads = .*/ParallelDownloads = 12/' /mnt/etc/pacman.conf
+                    sed -i '/^ParallelDownloads = 12/a Color\nILoveCandy' /mnt/etc/pacman.conf
 
-                genfstab -U /mnt > /mnt/etc/fstab
+                    genfstab -U /mnt > /mnt/etc/fstab
 
-                arch-chroot /mnt pacman-key --init || true
-                arch-chroot /mnt pacman-key --populate artix archlinux || true
-                arch-chroot /mnt pacman -Sy --noconfirm artix-mirrorlist artix-keyring archlinux-keyring || true
-                arch-chroot /mnt pacman -Sy --noconfirm artix-archlinux-support || true
+                    arch-chroot /mnt /bin/bash >> "$LOGFILE" 2>&1 <<EOF
+pacman-key --init
+pacman-key --populate artix archlinux
+pacman -Sy --noconfirm artix-mirrorlist artix-keyring archlinux-keyring artix-archlinux-support || true
 
-                DE_PKGS=""
-                DESKTOP_PKGS=""
-
-                if [ "$DE" == "1" ]; then
-                    DE_PKGS="plasma konsole dolphin"
-                    DESKTOP_PKGS="kitty fastfetch wl-clipboard sddm sddm-runit power-profiles-daemon power-profiles-daemon-runit pipewire pipewire-runit pipewire-pulse pipewire-pulse-runit wireplumber wireplumber-runit"
-                elif [ "$DE" == "2" ]; then
-                    DE_PKGS="xorg-server xfce4 xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin"
-                    DESKTOP_PKGS="kitty fastfetch sddm xclip maim sddm-runit power-profiles-daemon power-profiles-daemon-runit pipewire pipewire-runit pipewire-pulse pipewire-pulse-runit wireplumber wireplumber-runit"
-                fi
-
-                arch-chroot /mnt /bin/bash <<EOF
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -441,33 +416,46 @@ sed -i 's/^#*GRUB_DISTRIBUTOR=.*/GRUB_DISTRIBUTOR="Visnux"/' /etc/default/grub |
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Visnux || grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
+DE_PKGS=""
+DESKTOP_PKGS=""
+
+if [ "$DE" == "1" ]; then
+    DE_PKGS="plasma konsole dolphin"
+    DESKTOP_PKGS="kitty fastfetch wl-clipboard sddm sddm-runit power-profiles-daemon power-profiles-daemon-runit pipewire pipewire-runit pipewire-pulse pipewire-pulse-runit wireplumber wireplumber-runit"
+elif [ "$DE" == "2" ]; then
+    DE_PKGS="xorg-server xfce4 xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin"
+    DESKTOP_PKGS="kitty fastfetch sddm xclip maim sddm-runit power-profiles-daemon power-profiles-daemon-runit pipewire pipewire-runit pipewire-pulse pipewire-pulse-runit wireplumber wireplumber-runit"
+fi
+
 pacman -S \
-    $DE_PKGS \
-    $DESKTOP_PKGS \
+    \$DE_PKGS \
+    \$DESKTOP_PKGS \
     turnstile turnstile-runit \
     networkmanager networkmanager-runit \
     dbus dbus-runit \
     nano sudo \
     --noconfirm
 
+# Correct path for persistent runit service enabling
 mkdir -p /etc/runit/runsvdir/default
-mkdir -p /run/runit/service
 
-for svc in dbus elogind networkmanager turnstiled sddm power-profiles-daemon; do
+# Correct case-sensitivity for service folder names
+for svc in dbus elogind NetworkManager turnstiled sddm power-profiles-daemon; do
     if [ -d "/etc/runit/sv/\$svc" ]; then
         ln -sf "/etc/runit/sv/\$svc" /etc/runit/runsvdir/default/
-        ln -sf "/etc/runit/sv/\$svc" /run/runit/service/
     fi
 done
 EOF
-                [ $? -ne 0 ] && INIT_OK=false
+                    [ $? -ne 0 ] && INIT_OK=false
+                fi
+                rm -f "$ARTIX_CONF"
             fi
 
             if [ "$INIT_OK" == "true" ]; then
                 dialog --title "All done!" --msgbox "Installation complete! You can unmount /mnt and reboot into Visnux Linux." 0 0; clear
                 break
             else
-                dialog --title "Error" --msgbox "Installation encountered errors. Please check the terminal logs." 0 0; clear
+                show_error_log
             fi
         fi
     fi
